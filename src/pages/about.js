b6c1025139efa4800321ed03737142b0491c77be
Christopher <kriss17388+dev@gmail.com>
2023-03-06 19:09:37 +0000

    about page SSR?

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,35 +1,24 @@
 import axios from "axios";
-import React, { useState, useEffect } from "react";
-// import { Container } from "@chakra-ui/react";
-import styles from "@/styles/Home.module.css";
 import { Container, Heading } from "@chakra-ui/react";
+import styles from "@/styles/Home.module.css";
 
-export default function About() {
-  const [pageContent, setPageContent] = useState("");
-
-  useEffect(() => {
-    axios
-      .get("https://unlockyoursound.com/wp-json/wp/v2/pages/14091")
-      .then((response) => {
-        setPageContent(response.data.content.rendered);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, []);
-
+export default function About({ pageContent }) {
   return (
-    // <Container maxW="sm" bg="blue.600" color="white">
     <div className={styles.main}>
       <Container maxW="xl">
         <Heading>Christopher Carvalho</Heading>
-        {/* <h1>Page Content:</h1> */}
-        <div
-          style={{padding: '10px'}}
-          dangerouslySetInnerHTML={{ __html: pageContent }}
-        ></div>
+        <div style={{ padding: "10px" }} dangerouslySetInnerHTML={{ __html: pageContent }}></div>
       </Container>
     </div>
-    // </Container>
   );
+}
+
+export async function getServerSideProps() {
+  const response = await axios.get("https://unlockyoursound.com/wp-json/wp/v2/pages/14091");
+  const pageContent = response.data.content.rendered;
+  return {
+    props: {
+      pageContent,
+    },
+  };
 }
