5ee56f235528e547ecf8ef42d227660ae4d6cf8b
Christopher <kriss17388+dev@gmail.com>
2023-03-07 09:54:29 +0000

    menu text size

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,21 +7,22 @@ import { Divider } from "@chakra-ui/react";
 
 export default function Nav() {
   const { colorMode, toggleColorMode } = useColorMode();
+  const textSize = 17;
 
   return (
     <>
       <nav style={{ display: "flex", justifyContent: "space-between" }}>
         <Link href="/" legacyBehavior>
-          <a style={{ marginRight: "10px" }}>Home</a>
+          <a style={{ marginRight: "10px", fontSize: textSize }}>Home</a>
         </Link>
         <Link href="/about" legacyBehavior>
-          <a style={{ marginRight: "10px" }}>About</a>
+          <a style={{ marginRight: "10px", fontSize: textSize }}>About</a>
         </Link>
         <Link href="/posts" legacyBehavior>
-          <a style={{ marginRight: "10px" }}>Posts</a>
+          <a style={{ marginRight: "10px", fontSize: textSize }}>Posts</a>
         </Link>
         <Link href="/posts/featured-posts" legacyBehavior>
-          <a style={{ marginRight: "10px" }}>Featured</a>
+          <a style={{ marginRight: "10px", fontSize: textSize }}>Featured</a>
         </Link>
         <Button onClick={toggleColorMode} size={10} mr={10} mb={0}>
           {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
