e3bba34c6b68bb64595dbe5c657fc591a3cbc76f
Christopher <kriss17388+dev@gmail.com>
2023-03-08 10:53:37 +0000

    font styling

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,7 @@
 import '@/styles/globals.css'
 import { ChakraProvider } from '@chakra-ui/react'
 import theme from '@/theme'
+import "@fontsource/questrial";
 
 export default function App({ Component, pageProps }) {
 
