d718b039e5ae8923d1060097e2f8923c64b49940
Christopher <kriss17388+dev@gmail.com>
2023-03-08 11:07:24 +0000

    reverted font

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,27 +4,10 @@ import axios from "axios";
 import styles from "@/styles/Home.module.css";
 import Nav from "@/components/Nav";
 import { Divider } from "@chakra-ui/react";
-// import { useColorMode } from "@chakra-ui/react";
-// import { SunIcon, MoonIcon } from "@chakra-ui/icons";
 import Footer from "@/components/Footer";
-// import FeaturedPosts from "./posts/featured-posts";
-// import { Button } from "@chakra-ui/react";
 import Image from 'next/image';
 
 export default function Home({ posts }) {
-  // const { colorMode, toggleColorMode } = useColorMode();
-
-//   const GET_FEATURED_POSTS = `
-//   query GetFeaturedPosts {
-//     posts(first: 100, where: { categoryId: 322 }) {
-//       nodes {
-//         id
-//         slug
-//         title
-//       }
-//     }
-//   }
-// `;
 
   return (
     <div className={styles.main}>
@@ -51,25 +34,13 @@ export default function Home({ posts }) {
           ></iframe>
         </Center>
         <Divider mt={5} mb={5} />
-        {/* <div>
-          {posts.map((post) => (
-            <div key={post.slug}>
-              <Link href={`/posts/${post.slug}`}>
-                <Text as="u">{post.title.rendered}</Text>
-              </Link>
-              <Divider mt={5} mb={5} />
-            </div>
-          ))}
-        </div> */}
-        {/* <FeaturedPosts /> */}
-        {/* <Footer /> */}
       </Container>
       <div className={styles.main}>
         <Container maxW="xl">
           {/* <Nav /> */}
           <Heading>Featured Content</Heading>
           <Text>The hall of fame</Text>
-          <Divider mt={5} mb={5} />
+          <Divider mt={10} mb={5} />
           <ul>
             {posts.map((post) => (
               <div key={post.slug} >
@@ -81,7 +52,7 @@ export default function Home({ posts }) {
                     width={500}
                     height={500}
                   />
-                  <Divider mt={5} mb={5} />
+                  <Divider mt={10} mb={5} />
                 </Link>
               </div>
             ))}
@@ -93,19 +64,6 @@ export default function Home({ posts }) {
     </div>
   );
 }
-
-// export async function getStaticProps() {
-//   const response = await axios.get(
-//     "https://unlockyoursound.com/wp-json/wp/v2/posts"
-//   );
-//   const posts = response.data;
-
-//   return {
-//     props: {
-//       posts,
-//     },
-//   };
-// }
 
 const GET_FEATURED_POSTS = `
   query GetFeaturedPosts {
@@ -123,29 +81,6 @@ const GET_FEATURED_POSTS = `
     }
   }
 `;
-
-// function FeaturedPosts({ posts }) {
-//   return (
-//     <div className={styles.main}>
-//       <Container maxW="xl">
-//         <Nav />
-//         <Heading>Featured Posts</Heading>
-//         <Divider mt={5} mb={5} />
-//         <ul>
-//           {posts.map((post) => (
-//             <div key={post.slug}>
-//               <Link href={`/posts/${post.slug}`}>
-//                 <Text as="u">{post.title}</Text>
-//                 <Divider mt={5} mb={5} />
-//               </Link>
-//             </div>
-//           ))}
-//         </ul>
-//         <Footer />
-//       </Container>
-//     </div>
-//   );
-// }
 
 export async function getStaticProps() {
   const response = await axios.post("https://unlockyoursound.com/graphql", {
