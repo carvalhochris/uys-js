0ca483424226f23dab860d148629b3f083392126
Christopher <kriss17388+dev@gmail.com>
2023-03-08 21:01:14 +0000

    styling

--- src/pages/search.js
+++ src/pages/search.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import styles from "@/styles/Home.module.css";
-import { Container, Divider } from "@chakra-ui/react";
+import { Center, Container, Divider } from "@chakra-ui/react";
 import Link from "next/link";
 import Nav from "@/components/Nav";
 import { Input } from "@chakra-ui/react";
@@ -17,10 +17,8 @@ export default function SearchPage() {
     event.preventDefault();
     try {
       setLoading(true);
-      const response = await axios.post(
-        "https://unlockyoursound.com/graphql",
-        {
-          query: `
+      const response = await axios.post("https://unlockyoursound.com/graphql", {
+        query: `
             query SearchPosts($search: String!) {
               posts(where: { search: $search }) {
                 nodes {
@@ -31,9 +29,8 @@ export default function SearchPage() {
               }
             }
           `,
-          variables: { search: searchTerm },
-        }
-      );
+        variables: { search: searchTerm },
+      });
       setSearchResults(response.data.data.posts.nodes);
     } catch (error) {
       console.error(error);
@@ -54,20 +51,24 @@ export default function SearchPage() {
         <form onSubmit={handleSearch}>
           {/* <label>
             Input */}
-            <Input
-              type="text"
-              name="search"
-              value={searchTerm}
-              onChange={(e) => setSearchTerm(e.target.value)}
-              placeholder="find yourself"
-            />
+          <Input
+            type="text"
+            name="search"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            placeholder="find yourself"
+          />
           {/* </label> */}
           <br></br>
-          <Button type="submit" mt={5}>Search</Button>
+          <Button type="submit" mt={5}>
+            Search
+          </Button>
           <Divider />
         </form>
         {loading ? (
-          <Spinner size="xl" mt={10} />
+          <Center>
+            <Spinner size="xl" mt={10} />
+          </Center>
         ) : searchResults.length > 0 ? (
           searchResults.map((result) => (
             <div key={result.id}>
