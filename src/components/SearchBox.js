e7957762977c6b2677129dd88ef10dc45a80466e
Christopher <kriss17388+dev@gmail.com>
2023-03-09 11:24:44 +0000

    search box color mode

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -7,11 +7,14 @@ import Nav from "@/components/Nav";
 import { Input } from "@chakra-ui/react";
 import { Button } from "@chakra-ui/react";
 import { Spinner } from "@chakra-ui/react";
+import { useColorMode } from "@chakra-ui/react";
+// import Image from "next/image";
 
 export default function SearchPage() {
   const [searchTerm, setSearchTerm] = useState("");
   const [searchResults, setSearchResults] = useState([]);
   const [loading, setLoading] = useState(false);
+  const { colorMode } = useColorMode();
 
   const handleSearch = async (event) => {
     event.preventDefault();
@@ -46,13 +49,14 @@ export default function SearchPage() {
   return (
     <div>
       {/* <Container maxW="xl" maxH={20}> */}
-        {/* <Nav /> */}
-        {/* <h1>Search Page</h1> */}
-        <form onSubmit={handleSearch}>
-          {/* <label>
+      {/* <Nav /> */}
+      {/* <h1>Search Page</h1> */}
+      <form onSubmit={handleSearch}>
+        {/* <label>
             Input */}
-            <Heading>The Vault</Heading>
-            {/* <br></br> */}
+        <Heading>The Vault</Heading>
+        {/* <br></br> */}
+        {colorMode === "light" ? (
           <Input
             type="text"
             name="search"
@@ -60,36 +64,48 @@ export default function SearchPage() {
             onChange={(e) => setSearchTerm(e.target.value)}
             placeholder="lose yourself in our ever-growing archives"
             // size={100}
-            size='lg'
-            borderColor='#202020'
+            size="lg"
+            borderColor="#202020"
           />
-          {/* </label> */}
-          <br></br>
-          <br></br>
-
-          <Button type="submit" mt={5} size='lg' >
-            Search
-          </Button>
-          <Divider />
-        </form>
-        {loading ? (
-          <Center>
-            <Spinner size="xl" mt={10} />
-          </Center>
-        ) : searchResults.length > 0 ? (
-          searchResults.map((result) => (
-            <div key={result.id}>
-              <Link href={`/${result.slug}`}>
-                <h2 style={{ cursor: "pointer" }}>{result.title}</h2>
-              </Link>
-              <Divider />
-            </div>
-          ))
         ) : (
-          <p>
-            {/* ... */}
-            </p>
+          <Input
+            type="text"
+            name="search"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            _placeholder={{ color: 'white' }}
+
+            placeholder="lose yourself in our ever-growing archives"
+            // size={100}
+            size="lg"
+            borderColor="#ffffff"
+          />
         )}
+
+        {/* </label> */}
+        <br></br>
+        <br></br>
+        <Button type="submit" mt={5} size="lg">
+          Search
+        </Button>
+        <Divider />
+      </form>
+      {loading ? (
+        <Center>
+          <Spinner size="xl" mt={10} />
+        </Center>
+      ) : searchResults.length > 0 ? (
+        searchResults.map((result) => (
+          <div key={result.id}>
+            <Link href={`/${result.slug}`}>
+              <h2 style={{ cursor: "pointer" }}>{result.title}</h2>
+            </Link>
+            <Divider />
+          </div>
+        ))
+      ) : (
+        <p>{/* ... */}</p>
+      )}
       {/* </Container> */}
     </div>
   );
