a9b0e871ee99c6907cd50964f1f08df18ef05acf
Christopher <kriss17388+dev@gmail.com>
2023-03-09 14:13:09 +0000

    font decisions

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,15 +1,18 @@
-import { Html, Head, Main, NextScript } from 'next/document'
-import NavNavNav from '@/components/navnavnav'
+import { Html, Head, Main, NextScript } from "next/document";
+import NavNavNav from "@/components/navnavnav";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+<link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@500&display=swap" rel="stylesheet" /> */}
       {/* <NavNavNav /> */}
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
