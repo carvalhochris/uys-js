70f9ab6fff5b6deb1149bd3169e523c3065107ea
Christopher <kriss17388+dev@gmail.com>
2023-03-10 21:43:36 +0000

    meta tags

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,6 +5,11 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <title>Unlock Your Sound</title>
+      <meta property="title" content="Unlock Your Sound" key="title" />
+      <meta name="description" content="The Artist Self-Development Platform"></meta>
+      <meta property="og:title" content="Unlock Your Sound" />
+      <meta property="og:image" content="https://unlockyoursound.com/wp-content/uploads/2021/04/unlock-your-sound-blog-scaled.jpeg" />
       {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
 <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@500&display=swap" rel="stylesheet" /> */}
