55ccf739d0fed163bc2dfcaa7532c77fcce2bb2f
Christopher <kriss17388+dev@gmail.com>
2023-03-14 10:55:11 +0000

    cite styling

--- src/Apollo.js
+++
@@ -1,8 +0,0 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
-
-const client = new ApolloClient({
-  uri: "https://unlockyoursound.com/graphql",
-  cache: new InMemoryCache(),
-});
-
-export default client;
