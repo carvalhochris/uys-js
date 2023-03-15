c2fc81c2598fe543cba3b0b19390ca932a5d8512
Christopher <kriss17388+dev@gmail.com>
2023-03-15 19:28:20 +0000

    testing osano

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+      <script src="https://cmp.osano.com/16CPLWTYbBGRh2py/a484e5d0-73c0-4567-8ad3-f17119cb0c5c/osano.js"></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
