f576ec0c8cc88a63126da40fcbdfb12bee3c3ba9
Christopher <kriss17388+dev@gmail.com>
2023-03-16 12:08:55 +0000

    inital color mode light

--- src/theme.js
+++ src/theme.js
@@ -4,7 +4,7 @@ import { extendTheme } from "@chakra-ui/react";
 
 const theme = extendTheme({
   config: {
-    initialColorMode: "dark",
+    initialColorMode: "light",
     useSystemColorMode: true,
   },
   colors: {
