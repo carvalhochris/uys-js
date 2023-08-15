8507bb9af46b8376a1eea4fa29699aa53cbfffea
Christopher <kriss17388+dev@gmail.com>
2023-08-15 13:56:06 +0100

    conditional submit button

--- src/components/SubmitMix.js
+++ src/components/SubmitMix.js
@@ -1,6 +1,12 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { Spinner, Button, Input } from "@chakra-ui/react";
+import {
+  Spinner,
+  Button,
+  Input,
+  FormControl,
+  FormLabel,
+} from "@chakra-ui/react";
 
 function SubmitMix() {
   //   const [inputs, setInputs] = useState({});
@@ -11,12 +17,15 @@ function SubmitMix() {
   const [isPosting, setIsPosting] = useState(false);
   const [discValue, setDiscoverValue] = useState("");
   const [showSuccess, setShowSuccess] = useState(false);
+  const [isError, setIsError] = useState(false);
 
   useEffect(() => {
     setIsPosting(false);
+    setIsError(false);
   }, []);
 
   const handleName = (event) => {
+    handleError();
     const eventname = event.target.value;
     setNameValue(eventname);
     // setNameValue = event.target.name;
@@ -24,6 +33,7 @@ function SubmitMix() {
   };
 
   const handleEmail = (event) => {
+    handleError();
     const eventemail = event.target.value;
     setEmailValue(eventemail);
     // setNameValue = event.target.name;
@@ -31,21 +41,34 @@ function SubmitMix() {
   };
 
   const handleLink = (event) => {
+    handleError();
     const eventlink = event.target.value;
     setLinkValue(eventlink);
     console.log(linkValue);
   };
 
   const handleDesc = (event) => {
+    handleError();
     const eventdesc = event.target.value;
     setDescValue(eventdesc);
     console.log(descValue);
   };
 
   const handleDiscover = (event) => {
+    handleError();
     const eventdisc = event.target.value;
     setDiscoverValue(eventdisc);
     console.log(discValue);
+  };
+
+  const handleError = () => {
+    if (nameValue && emailValue && linkValue && descValue && discValue) {
+      console.log("good");
+      setIsError(true);
+    } else {
+      console.log("bad");
+      setIsError(false);
+    }
   };
 
   const handleSubmit = () => {
@@ -60,16 +83,18 @@ function SubmitMix() {
     formData.append("disc", discValue);
     console.log("formData is...");
     console.log(formData);
+
     setTimeout(() => {
       console.log("Delayed for 1 second.");
       setIsPosting(false);
       setShowSuccess(true);
-      axios.post('https://service.songcards.io/submit-mix', formData);
+      // axios.post("https://service.songcards.io/submit-mix", formData);
     }, 1000);
-
     // alert('sent!')
     // alert(inputs);
   };
+
+  // const isError =
 
   return (
     <>
@@ -79,27 +104,32 @@ function SubmitMix() {
       // onSubmit={handleSubmit}
       >
         <label>
-          Enter your name:
-          <Input
-            type="text"
-            // value={nameValue}
-            // name="name"
-            // value={inputs.username || ""}
-            onChange={handleName}
-          />
+          <FormControl isRequired>
+            <FormLabel>Enter your name:</FormLabel>
+            <Input
+              type="text"
+              // value={nameValue}
+              // name="name"
+              // value={inputs.username || ""}
+              onChange={handleName}
+            />
+          </FormControl>
         </label>
         <br />
         <br />
-        <label>
-          Enter your email:
+        {/* <label> */}
+        <FormControl isRequired>
+          <FormLabel>Enter your email:</FormLabel>
           <Input
             type="text"
+            // isRequired
             // name="email"
             // value={inputs.age || ""}
             onChange={handleEmail}
           />
-        </label>
-        <br />
+        </FormControl>
+        {/* </label> */}
+        {/* <br /> */}
 
         {/* <label>
           What best describes you? (Artist, Producer, Engineer?):
@@ -131,16 +161,18 @@ function SubmitMix() {
           />
         </label>
         <br /> */}
-        <label>
-          Tell us a bit about you and the project
+        {/* <label> */}
+        <FormControl isRequired>
+          <FormLabel>Tell us a bit about you and the project</FormLabel>
           <Input
             type="text"
             // name="age"
             // value={inputs.age || ""}
             onChange={handleDesc}
           />
-        </label>
-        <br />
+        </FormControl>
+        {/* </label> */}
+        {/* <br /> */}
         <br />
         {/* <label>
           What is your biggest obstacle at this time?
@@ -152,28 +184,37 @@ function SubmitMix() {
           />
         </label>
         <br /> */}
-        <label>
-          How did you discover Unlock Your Sound?
+        {/* <label> */}
+        <FormControl isRequired>
+          <FormLabel>How did you discover Unlock Your Sound?</FormLabel>
           <Input
             type="text"
             // name="age"
             // value={inputs.age || ""}
             onChange={handleDiscover}
           />
-        </label>
+        </FormControl>
+        {/* </label> */}
+        {/* <br /> */}
         <br />
-        <br />
-        <label>
-          Please provide a link to your track here
+        {/* <label> */}
+        <FormControl isRequired>
+          <FormLabel>Please provide a link to your track here</FormLabel>
           <Input
             type="text"
             // name="age"
             // value={inputs.age || ""}
             onChange={handleLink}
           />
-        </label>
+        </FormControl>
+        {/* </label> */}
         <br />
         {/* <input type="submit" /> */}
+      </form>
+      {isPosting && <Spinner />}
+      {showSuccess && <p>sent!</p>}
+      {!isError && <p>fill in required fields</p>}
+      {isError && (
         <Button
           onClick={handleSubmit}
           mt={10}
@@ -187,9 +228,7 @@ function SubmitMix() {
         >
           Submit Mix
         </Button>
-      </form>
-      {isPosting && <Spinner />}
-      {showSuccess && <p>sent!</p>}
+      )}
     </>
   );
 }
