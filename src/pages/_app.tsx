b1b7ae2a228af4fc3e466a91e19ba9e87158662d
Christopher <kriss17388+dev@gmail.com>
2023-12-01 22:51:14 +0000

    next app props

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,8 +4,10 @@ import { ChakraProvider } from '@chakra-ui/react'
 import theme from '../theme';
 import "@fontsource/questrial";
 import React from 'react';
+import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }) {
+
+export default function App({ Component, pageProps }: AppProps) {
 
   return (
     <ChakraProvider theme={theme}>
