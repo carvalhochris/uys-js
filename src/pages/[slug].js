d1e5ab7b0f30984ef372f24a65e209f752779db2
Christopher <kriss17388+dev@gmail.com>
2023-12-01 17:13:50 +0000

    5

--- src/pages/[slug].js
+++ src/pages/[slug].js
@@ -9,7 +9,7 @@ import Footer from "@/components/Footer";
 import Image from "next/image";
 import ShareButton from "@/components/WhatsAppButton";
 import Head from "next/head";
-import { useColorMode } from '@chakra-ui/react';
+import { useColorMode } from "@chakra-ui/react";
 
 const GET_POST_BY_SLUG = `
   query GetPostBySlug($slug: String!) {
@@ -32,8 +32,8 @@ const GET_POST_BY_SLUG = `
 
 export default function Post({ post }) {
   const { colorMode } = useColorMode();
-  const textColor = colorMode === 'dark' ? 'gray.100' : 'gray.900';
-  const headingColor = colorMode === 'dark' ? 'white' : 'gray.900';
+  const textColor = colorMode === "dark" ? "gray.100" : "gray.900";
+  const headingColor = colorMode === "dark" ? "white" : "gray.900";
 
   return (
     <>
@@ -98,10 +98,13 @@ export default function Post({ post }) {
                 height={2000}
               />
             )}
-            <Heading as="h1" color={headingColor} >
-        {post.title}
-      </Heading>
-      <Text color={textColor} dangerouslySetInnerHTML={{ __html: post.content }} />
+            <Heading as="h1" color={headingColor}>
+              {post.title}
+            </Heading>
+            <Text
+              color={textColor}
+              dangerouslySetInnerHTML={{ __html: post.content }}
+            />
             <br></br>
             <ShareButton postSlug={post.slug} />
             <iframe
@@ -115,8 +118,8 @@ export default function Post({ post }) {
               }}
               frameborder="0"
               scrolling="no"
-              ></iframe>
-              <br></br>
+            ></iframe>
+            <br></br>
           </Box>
           <Footer />
         </Container>
@@ -133,9 +136,7 @@ export async function getStaticProps({ params }) {
     variables: { slug },
   });
 
-  setTimeout(() => {
-    console.log("Props delayed for 21 seconds.");
-  }, 21000);
+  setTimeout(console.log("Props delayed by 5 seconds"), 5);
 
   const post = response.data.data.postBy;
 
