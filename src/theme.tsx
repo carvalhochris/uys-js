083a294d9c7b9bb91dc5ef27614be625664519c6
Christopher Carvalho <christophercarvalho@Christophers-MacBook-Pro.local>
2024-01-29 16:10:25 +0000

    initial color mode system

--- src/theme.tsx
+++ src/theme.tsx
@@ -4,7 +4,7 @@ import { extendTheme } from "@chakra-ui/react";
 
 const theme = extendTheme({
   config: {
-    initialColorMode: "light",
+    initialColorMode: "system",
     useSystemColorMode: true,
   },
   colors: {
