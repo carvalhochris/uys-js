75adffde6789b208a2682890abb4c47fe9bc222a
Christopher Carvalho <christophercarvalho@Christophers-MacBook-Pro.local>
2024-04-25 15:40:12 +0100

    sitemap exclude

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,5 +2,5 @@
 module.exports = {
   siteUrl: process.env.SITE_URL || 'https://unlockyoursound.com',
   generateRobotsTxt: true, // (optional)
-  // ...other options
+  exclude: ['/rss', '/wordpress', 'wp-cron.php', 'wp-login.php', 'xmlrpc.php'],
 }
